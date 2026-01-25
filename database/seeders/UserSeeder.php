50c170c63d4b711d58c29f1b3451ab1a407ca8ec
AungKoThet2k7 <aungkothat.mgn19@gmail.com>
2026-01-25 09:10:03 +0630

    feat : Auth, Profile

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = [
+            [
+                'name' => 'Admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('asdffdsa'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ];
+
+        User::insert($user);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,33 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('v1')->group(function () {
+
+    // index
+    Route::get('/', function () {
+        return response()->json([
+            'message' => 'Welcome to POS API',
+        ]);
+    });
+
+    // Auth
+    Route::controller(AuthController::class)->group(function () {
+        Route::post('/register', [AuthController::class, 'register']);
+        Route::post('/login', [AuthController::class, 'login']);
+    });
+
+    Route::group(['middleware' => ['auth:sanctum']], function () {
+
+        // Profile
+        Route::controller(ProfileController::class)->prefix('profile')->group(function () {
+            Route::get('/show', 'show');
+            Route::patch('/change-name', 'changeName');
+            Route::patch('/change-password', 'changePassword');
+            Route::patch('/change-photo', 'changePhoto');
+            Route::post('/logout', 'logout');
+        });
+    });
+});
